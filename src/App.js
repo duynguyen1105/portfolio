c51d41e07204fc9dd73b27e229c15982de06b9c4
duynguyenhhg <nguyen.nguyen@hellohealthgroup.com>
2022-07-12 00:48:04 +0700

    create

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Learn Reacta
+        </a> 
       </header>
     </div>
   );
